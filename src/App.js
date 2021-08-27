d835703ad0b127447bb9ecbd38b78ae4adbe0feb
Saurish Kar <basicallysaurish@gmail.com>
2021-08-27 21:11:49 +0530

    minor change

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
   return (
     <div className="App">
       <AppContext.Provider value={{ session, setSession }}>
-        <BrowserRouter basename="/home">
+        <BrowserRouter>
           <Switch>
             <Route
               exact
