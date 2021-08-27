1786fce154e5b1640d792eb586330916dbc30147
Rahman24 <50796694+Rahman24@users.noreply.github.com>
2021-08-27 11:00:02 +0530

    changed contact us in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,16 +3,16 @@ import wnkLogo from "assets/images/wnk-logo.png";
 import "./Footer.css";
 
 const Footer = () => {
+  let contact-url="mailto:21f1005287@student.onlinedegree.iitm.ac.in"
   return (
     <footer className="py-5 mt-5 mx-5">
       <div className="row">
         <div className="col-3 contact-us-section">
           <h4 className="text-uppercase text-white">Contact us</h4>
           <p className="text-color-green">
-            “Lorem ipsum dolor sit amet,”
+            <a href={url}>Noufal Rahman</a>
           </p>
         </div>
-        <div className="col-6"></div>
         <div className="col-3 text-end logo">
           <img src={wnkLogo} alt="wnk logo" />
         </div>
