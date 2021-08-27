22ab593e0ef9b5a5720d1e4de5453efbd9d23895
Rahman24 <50796694+Rahman24@users.noreply.github.com>
2021-08-27 11:19:59 +0530

    Feature / Events List - Added dummy events and event card component (#2) (#8)
    * added css
    
    * added css
    
    * changed contact us in footer
    
    * changes contact us in footer
    
    * minor change
    
    Co-authored-by: Saurish Kar <basicallysaurish@gmail.com>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@ import wnkLogo from "assets/images/wnk-logo.png";
 import "./Footer.css";
 
 const Footer = () => {
+  let contact_url="mailto:21f1005287@student.onlinedegree.iitm.ac.in"
   return (
     <footer className="py-5 mt-5 px-5 ">
       <hr className="line" />
@@ -10,10 +11,9 @@ const Footer = () => {
         <div className="col-3 contact-us-section">
           <h4 className="text-uppercase text-white">Contact us</h4>
           <p className="text-color-green">
-            “Lorem ipsum dolor sit amet,”
+            <a href={contact_url}>Noufal Rahman</a>
           </p>
         </div>
-        <div className="col-6"></div>
         <div className="col-3 text-end logo">
           <img src={wnkLogo} alt="wnk logo" />
         </div>
