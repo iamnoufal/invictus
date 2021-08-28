9a82aa872c0a26171f48eee246f437e47ec9c65b
Saurish Kar <basicallysaurish@gmail.com>
2021-08-28 15:37:12 +0530

    Removed unused code

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,31 +1,6 @@
-import { useContext, useEffect, useState } from "react";
-import { useHistory } from "react-router";
-
 import Layout from "components/Layout";
-import AuthButton from "components/button/Auth";
-import Loader from "components/Loader";
-
-import { AppContext } from "contexts/app";
-
-import homeLg from "assets/images/home_lg.png";
-import homeMob from "assets/images/home_mob.png";
 
 const HomePage = () => {
-  
-
-  // const checkBg = () => {
-  //   if (document.body.offsetWidth > 1000) {
-  //     bgimg = "https://rahman24.github.io/invictus/backup/main-desk.png";
-  //     document.querySelector(".bg-img").src = bgimg;
-  //   } else {
-  //     bgimg = "https://rahman24.github.io/invictus/backup/main-mob.png";
-  //     document.querySelector(".bg-img").src = bgimg;
-  //   }
-  // };
-  // checkBg();
-  // window.addEventListener("resize", () => {
-  //   checkBg();
-  // });
   return (
     <Layout>
       <img
