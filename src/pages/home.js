46c21097a8f10450a9646c0d263f154c739ff548
Saurish Kar <basicallysaurish@gmail.com>
2021-08-28 14:53:15 +0530

    minor bg image changes

--- src/pages/home.js
+++ src/pages/home.js
@@ -32,20 +32,31 @@ const HomePage = () => {
     }
   }, [session]);
 
-  const checkBg = () => {
-    if (document.body.offsetWidth>1000) {
-      bgimg="https://rahman24.github.io/invictus/backup/main-desk.png";
-      document.querySelector(".bg-img").src=bgimg;
-    } else {
-      bgimg="https://rahman24.github.io/invictus/backup/main-mob.png";
-      document.querySelector(".bg-img").src=bgimg
-    }
-  }
-  checkBg();
-  window.addEventListener("resize",()=>{checkBg()});
+  // const checkBg = () => {
+  //   if (document.body.offsetWidth > 1000) {
+  //     bgimg = "https://rahman24.github.io/invictus/backup/main-desk.png";
+  //     document.querySelector(".bg-img").src = bgimg;
+  //   } else {
+  //     bgimg = "https://rahman24.github.io/invictus/backup/main-mob.png";
+  //     document.querySelector(".bg-img").src = bgimg;
+  //   }
+  // };
+  // checkBg();
+  // window.addEventListener("resize", () => {
+  //   checkBg();
+  // });
   return (
     <Layout>
-      <img src={bgimg} class="bg-img" />
+      <img
+        src="https://rahman24.github.io/invictus/backup/main-desk.png"
+        className="bg-img d-none d-md-block d-lg-block"
+        alt="home page"
+      />
+      <img
+        src="https://rahman24.github.io/invictus/backup/main-mob.png"
+        className="bg-img d-block d-md-none d-lg-none"
+        alt="home page"
+      />
 
       {/* <div className="home-page">
         <h2 className="text-center text-white text-uppercase my-5 heading">Home page</h2>
