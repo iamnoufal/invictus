ae8f76a1fb1bc0a577905ce320a7ac67fb77d4bd
Rahman24 <50796694+Rahman24@users.noreply.github.com>
2021-08-28 10:58:46 +0530

    ui fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => {
         <div className="col-3 contact-us-section">
           <h4 className="text-uppercase text-white">Contact us</h4>
           <p className="text-color-green">
-            <a className=".text-color-green" href={contact_url}>Noufal Rahman</a>
+            <a className="text-color-green" href={contact_url}>Noufal Rahman</a>
           </p>
         </div>
         <div className="col-9 text-end logo">
