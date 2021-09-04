5d4a7dc60a91e02a9fb8ae1d62f6327f125a0ebb
Saurish Kar <basicallysaurish@gmail.com>
2021-09-04 21:16:02 +0530

    minor change

--- src/components/User.js
+++ src/components/User.js
@@ -3,7 +3,7 @@ import "./User.css";
 const User = ({ img, name, contact = {}, role = "", desc = "", ContactComponent }) => {
   return (
     <div className="user-card py-4 px-2">
-      <img src={img} alt="user-profile" className="img-circle rounded-50" />
+      <img src={img} alt="user-profile" className="profile-img img-circle rounded-50" />
       <div>
         <h3 className="text-white fw-bold">{name}</h3>
         <h5><span className="badge bg-primary d-block my-3 text-uppercase">{role}</span></h5>
